aeaf72c21cb6f3f2e24987ac9fd137c28bec493b
BreacherBros <breacherbros@gmail.com>
2026-01-23 03:16:42 -0800

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -4,20 +4,18 @@ import fetch from "node-fetch";
 const router = express.Router();
 
 const YT_API_KEY = process.env.YOUTUBE_API_KEY;
+const CHANNEL_ID = "UCBkzbmUXRMiwfb2yeV9iuyQ"; 
+const UPLOADS_PLAYLIST = "UUBkzbmUXRMiwfb2yeV9iuyQ";
 
-// BreacherBros IDs
-const CHANNEL_ID = "UCBkzbmUXRMiwfb2yeV9iuyQ";
-const UPLOADS_PLAYLIST_ID = "UUBkzbmUXRMiwfb2yeV9iuyQ";
-
-// Test
+/* TEST */
 router.get("/test", (req, res) => {
   res.json({ ok: true, msg: "youtube route works" });
 });
 
-// Neuestes Video (sauber, stabil, offiziell)
+/* LATEST VIDEO */
 router.get("/youtube-latest", async (req, res) => {
   try {
-    const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${UPLOADS_PLAYLIST_ID}&maxResults=1&key=${YT_API_KEY}`;
+    const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${UPLOADS_PLAYLIST}&maxResults=1&key=${YT_API_KEY}`;
 
     const r = await fetch(url);
     const data = await r.json();
@@ -31,7 +29,7 @@ router.get("/youtube-latest", async (req, res) => {
     res.json({
       videoId: v.resourceId.videoId,
       title: v.title,
-      thumbnail: v.thumbnails.high.url,
+      thumbnail: v.thumbnails.maxres?.url || v.thumbnails.high.url,
       published: v.publishedAt
     });
 
