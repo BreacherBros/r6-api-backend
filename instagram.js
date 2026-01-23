3f256e6d8446b482659f950613606c53b2c31101
BreacherBros <breacherbros@gmail.com>
2026-01-23 08:12:16 -0800

    Update instagram.js

--- instagram.js
+++ instagram.js
@@ -11,32 +11,67 @@ const USERNAME = "breacherbros";
 ========================= */
 router.get("/instagram-latest", async (req, res) => {
   try {
-    const url = `https://ensembledata.com/apis/instagram/user/reels?username=${USERNAME}&chunk_size=1&token=${INSTA_API_KEY}`;
+    const url = `https://ensembledata.com/apis/instagram/user/reels?username=${USERNAME}&chunk_size=5&token=${INSTA_API_KEY}`;
 
     const r = await fetch(url);
     const data = await r.json();
 
-    if (!data?.data?.reels || data.data.reels.length === 0) {
+    /* =========================
+       RAW DATA DEBUG (optional)
+    ========================= */
+    // console.log(JSON.stringify(data, null, 2));
+
+    let mediaList = [];
+
+    // mögliche Datenpfade
+    if (data?.data?.reels?.length) mediaList = data.data.reels;
+    else if (data?.data?.items?.length) mediaList = data.data.items;
+    else if (data?.data?.media?.length) mediaList = data.data.media;
+
+    if (!mediaList.length) {
       return res.status(404).json({ error: "No Instagram media found" });
     }
 
-    const reel = data.data.reels[0];
+    // 🔥 neuestes Reel/Video finden
+    const reel =
+      mediaList.find(m => m.media_type === 2 || m.video_versions) ||
+      mediaList[0];
 
-    const shortcode = reel.shortcode;
-    const caption = reel?.caption?.text || "";
+    const shortcode =
+      reel?.shortcode ||
+      reel?.code ||
+      reel?.id ||
+      null;
+
+    const caption =
+      reel?.caption?.text ||
+      reel?.caption ||
+      "";
+
     const thumbnail =
-      reel?.image_versions2?.candidates?.[0]?.url || null;
+      reel?.image_versions2?.candidates?.[0]?.url ||
+      reel?.thumbnail_url ||
+      reel?.display_url ||
+      null;
 
     const video_url =
-      reel?.video_versions?.[0]?.url || null;
+      reel?.video_versions?.[0]?.url ||
+      reel?.video_url ||
+      null;
+
+    if (!shortcode && !video_url) {
+      return res.status(404).json({ error: "No usable reel found" });
+    }
 
     res.json({
-      id: reel.id,
+      id: reel.id || null,
       shortcode,
       video_url,
       thumbnail,
       caption,
-      permalink: `https://www.instagram.com/reel/${shortcode}/`
+      permalink: shortcode
+        ? `https://www.instagram.com/reel/${shortcode}/`
+        : null
     });
 
   } catch (err) {
